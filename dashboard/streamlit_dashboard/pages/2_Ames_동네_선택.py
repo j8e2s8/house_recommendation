8ec75402191fa322e70ae5ca5bd25cb932168539
정은서 <j8e2s8@sookmyung.ac.kr>
2025-07-23 00:23:36 +0900

    modified streamlit

--- dashboard/streamlit_dashboard/pages/2_Ames_동네_선택.py
+++ dashboard/streamlit_dashboard/pages/2_Ames_동네_선택.py
@@ -97,22 +97,22 @@ if buttons[0] == 'g_grlivarea':
     eda_df['g_grlivarea'] = pd.Categorical(eda_df['g_grlivarea'], categories=['(0, 500]', '(500, 1000]', '(1000, 1500]', '(1500, 2000]', '(2000, 2500]', '(2500, 3000]', '(3000, 3500]', '(3500, 4000]', '(4000, 4500]', '(4500, 5000]', '(5000, 5500]', '(5500, 6000]'])
     g2 = pd.DataFrame(eda_df[eda_df['cluster'].isin(selected)].value_counts(['g_grlivarea','cluster']))
     pivot_df2 = g2.pivot_table(index=buttons[0], columns='cluster', values='count')
-    st.bar_chart(pivot_df2, height=300)
+    st.bar_chart(pivot_df2, height=400)
 elif buttons[0] == 'Year_Remod_Add':
     g3 = eda_df[eda_df['cluster'].isin(selected)].groupby([buttons[0],'cluster'], as_index=False).agg(counts=(buttons[0],'count'))
     pivot_df3 = g3.pivot_table(index=buttons[0],columns='cluster',values='counts')
-    st.line_chart(pivot_df3, height=300)
+    st.line_chart(pivot_df3, height=400)
 elif buttons[0] == 'total':
     g4 = score_df[(score_df['cluster'].isin(selected)) & (score_df['Paved_Drive']=='Paved')& ((score_df['Fence']=='Good_Privacy') | (score_df['Fence']=='Minimum_Privacy'))].sort_values('total',ascending=False).head(7).reset_index(drop=True)
     pivot_df4 = g4.pivot_table(index='Unnamed: 0', columns='cluster', values='total').sort_values(0,ascending=False)
     pivot_df4.index.name = 'index'
     pivot_df4['max_score'] = pivot_df4.max(axis=1)
     pivot_df4 = pivot_df4.sort_values('max_score', ascending=False).drop(columns='max_score')
-    st.bar_chart(pivot_df4, height=300)
+    st.bar_chart(pivot_df4, height=400)
 else:
     g1 = eda_df[eda_df['cluster'].isin(selected)].groupby([buttons[0],'cluster'], as_index=False).size()
     pivot_df1 = g1.pivot_table(index=buttons[0],columns='cluster',values='size')
-    st.bar_chart(pivot_df1, height=300)
+    st.bar_chart(pivot_df1, height=400)
 
 
 st.write('')
