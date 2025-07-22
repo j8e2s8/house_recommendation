6da12c2b16760598d569bf4032e151d5dcf91f6e
정은서 <j8e2s8@sookmyung.ac.kr>
2025-07-23 00:10:49 +0900

    modified streamlit

--- dashboard/streamlit_dashboard/pages/1_Ames_동네_분석.py
+++ dashboard/streamlit_dashboard/pages/1_Ames_동네_분석.py
@@ -30,9 +30,11 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Ames 동네별 학교와 병원의 위치")
     st.write('''**율이를 위해 학교와 병원의 위치 알기**
+    \n 
     \n점 마크 : 주택 (동네별로 색 구분), 
     \n학교 핀 마크 : 교육기관 (darkgreen색), 
-    \n병원 핀 마크 : 의료기관 (orange색)''')
+    \n병원 핀 마크 : 의료기관 (orange색)
+    \n ''')
     with open("dashboard/img/Ames_map.html", "r", encoding="utf-8") as f:
         st.components.v1.html(f.read(), height=500, scrolling=False)
 
